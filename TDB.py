c4de5ecbdba7ba89ab5a1da08643001303c5fdb9
Arty6060 <43887613+Arty6060@users.noreply.github.com>
2019-01-21 20:27:45 +0000

    3rd Update
    Finished and fixed up the Thrall encounter, and commented the code for it. Learned that the issue was I forgot to import random, which is essential. Will now start on the second encounter and finish for tomorrow.

--- TDB.py
+++ TDB.py
@@ -2,13 +2,20 @@
 partOne=True
 partTwo=True
 partThree=True
+partFour=True
+partFive=True
+partSix=True
 abilities = {"Nova Bomb": 1, "Axion Bolt": 2}
 inventory = {"MIDA": False, "LDR": False}
 Thrall=5
-thrallTimer=5
-Ogre=10
+Acolytes=8
+Knight=5
+Ogre=15
+battleOne=5
+battleTwo=7
 midaHit=0
 ldrHit=0
+import random
 
 ##Intro
 print("---The Dark Below: A Destiny Text Adventure---")
@@ -67,16 +74,22 @@ while partTwo:
     else:
         print("-Invalid action, please try again-")
 
-print("Your Ghost begins deciphering the rune. He estimates it'll take a minute. Halfway through you begin to feel eyes gazing from the shadows. A Thrall blindly screeches at you, piercing your ears. Three more jump out alongside it and dash towards you.")
+print("Your Ghost begins deciphering the rune. He estimates it'll take a minute. Halfway through you begin to feel eyes gazing from the shadows. A Thrall blindly screeches at you, piercing your ears. Four more jump out alongside it and dash towards you.")
 #Part Three (Thrall ambush)
 while partThree:
-    if thrallTimer != 0:
+
+    if Thrall <= 0:
+        print("You've eliminated all the Thrall, and your Ghost has finished dismantling the rune. The chamber door creeks open. You pass through.")
+        partThree=False
+
+    elif battleOne != 0:
         print("You can:")
         print("-Ready weapon (if not already ready)-")
         print("-Fire equipped weapon-")
         print("-Use a Void ability-")
         print("Thrall left: " , Thrall)
 
+            ##Equip Weapon tree
         inputThree=input("What will you do? >>> ").lower()
         if inputThree in ["ready weapon", "equip weapon"]:
             inputWeapon=input("Which weapon do you equip? >>> ").lower()
@@ -84,17 +97,17 @@ while partThree:
                 print("MIDA Multi-Tool equipped.")
                 inventory["MIDA"] = True
                 inventory["LDR"] = False
-                thrallTimer -= 1
+                battleOne -= 1
 
             elif inputWeapon in ["ldr", "ldr-5001", "sniper rifle"]:
                 print("LDR-5001 equipped.")
                 inventory["MIDA"] = False
                 inventory["LDR"] = True
-                thrallTimer -= 1
+                battleOne -= 1
 
             else:
                 print("No weapon equipped.")
-
+                ##Void Ability tree
         elif inputThree in ["use void", "void", "use void ability", "use ability"]:
             inputVoid=input("Which ability do you use? >>> ").lower()
             if inputVoid in ["axion bolt", "bolt", "grenade"]:
@@ -102,33 +115,55 @@ while partThree:
                     print("Your Axion Bolt is flung in front of you, roughly in the direction of the incoming Thrall. 2 bolts spawn in the radius and seek out two of the Thrall. They erupt in a void explosion.")
                     Thrall -= 2
                     print(Thrall)
-                    thrallTimer -= 2
+                    battleOne -= 2
                     abilities["Axion Bolt"] -= 1
                     print(abilities)
+                    continue
 
-                if Thrall == 1:
+                elif Thrall == 1:
                     print ("Your Axion Bolt is flung in front of you, in the direction of the last Thrall. It stumbles at the sight of your grenade, aware of its power. The bolt promptly consumes the creature, killing it.")
                     Thrall -= 1
                     abilities["Axion Bolt"] -= 1
                     print(abilities)
+                    continue
 
+            elif inputVoid in ["nova bomb", "nova", "bomb", "void bomb", "one round purple boi"]:
+                print("You jump up and summon an intense orb of Void energy between your palms, proppelling it towards the incoming Thrall. They don't stand a chance. The bomb decimates them, leaving no trace.")
+                Thrall -= 5
 
+            else:
+                print("-Invalid action, please try again-")
+
+                    ##Fire Weapon tree
         elif inputThree in ["fire weapon", "fire", "use weapon"]:
             if inventory["MIDA"] == True:
                 if Thrall >= 3:
-                    midaHit.randint(1,3)
+                    midaHit = random.randint(1, 3)
                     print("You fire off a volley of shots, killing " , midaHit , " Thrall. Their heads explode in a fountain of inky darkness.")
                     Thrall -= midaHit
-                    thrallTimer -= 1
-                if Thrall == 2:
-                    midaHit.randint(1,2)
+                    battleOne -= 1
+                    continue
+                elif Thrall == 2:
+                    midaHit = random.randint(1, 2)
                     print("You fire off a volley of shots, killing " , midaHit , " Thrall. Their heads explode in a fountain of inky darkness.")
                     Thrall -= midaHit
-                    thrallTimer -= 1
-                if Thrall == 1:
-                    midaHit = 1
-                    print("The last Thrall scuttles towards you, and with a well placed shot, you pop off its head. You reload your clip")
-
+                    battleOne -= 1
+                    continue
+                elif Thrall == 1:
+                    print("The last Thrall scuttles towards you, and with a well placed shot, you pop off its head. You reload your clip.")
+                    Thrall -= 1
+                    continue
+            elif inventory["LDR"] == True:
+                if Thrall != 1:
+                    print("You nail one of the Thrall in the head with a clean shot. Dark essence pours out as it dissolves from reality.")
+                    Thrall -= 1
+                    battleOne -= 1
+                    continue
+                elif Thrall == 1:
+                    print("The last Thrall charges towards you, and without scoping in, you take a point blank shot at its head. Your bullet rings loudly through the valley. You reload your clip.")
+                    continue
+            else:
+                print("-You have no weapon equipped-")
 
 
         else:
@@ -137,5 +172,23 @@ while partThree:
     else:
         print("The Thrall close in and incapacitate you, leaving your corpse mauled and tattered after feasting on your light.")
         print("THE DARKNESS CONSUMED YOU")
-        thrallTimer = 5
+        battleOne = 5
+        Thrall = 5
+        abilities["Axion Bolt"] = 2
+        abilities["Nova Bomb"] = 1
         continue
+
+print("You move slowly through the tomb, past the chamber. All of a sudden you see two blaster shots from opposite directions coming towards you. You dodge out of the way, and get a glimpse at your foes. Two Hive Acolytes and a Knight.")
+##Part Four (Acolyte battle)
+while partFour:
+    if Acolytes <= 0 and Knight <= 0:
+            print("You've cleared the wave of enemies. You make your way deeper into the tomb.")
+            partFour=False
+
+    elif battleTwo != 0:
+        print("You can:")
+        print("-Ready Weapon-")
+        print("-Use Void Ability-")
+        print("-Talk to your Ghost-")
+        print("-Take Cover-")
+        inputFour = input("What will you do? >>> ").lower()
