3de79baac62fce57a92e1c5a2c3bfc071e9c07ab
Arty6060 <43887613+Arty6060@users.noreply.github.com>
2019-01-23 22:20:59 +0000

    4th Update
    Coding the second encounter is proving to be more challenging than expected. Tackling three different enemies with different health values and making sure the story fits what's being done in the code without any errors or holes in logic is both time-consuming and straining. What coding this encounter has forced me to do, however, is to think about situations in a game through a logic of checks and numbers. Now when I'm looking at something like "X enemy dies" in my code, I see the values being changed in the code and not just the words being printed. I've completed the bulk of the work on this segment though, and the rest of the code from here on out should be much more straightforward now that I've tackled this challenge.

--- TDB.py
+++ TDB.py
@@ -8,11 +8,11 @@ partSix=True
 abilities = {"Nova Bomb": 1, "Axion Bolt": 2}
 inventory = {"MIDA": False, "LDR": False}
 Thrall=5
-Acolytes=8
-Knight=5
+battleTwoEnemies = {"AcolyteOne": 2, "AcolyteTwo": 2, "Knight": 5}
 Ogre=15
 battleOne=5
 battleTwo=7
+cover=False
 midaHit=0
 ldrHit=0
 import random
@@ -49,25 +49,26 @@ while partTwo:
     print("-Check equipment-")
     print("-Ready weapon-")
     inputTwo=input("What will you do? >>> ").lower()
+
+            ##Equipment
     if inputTwo in ["check equipment", "equipment", "look equipment", "inspect equipment"]:
         print("You are armed with a MIDA Multi-Tool Scout Rifle and a LDR-5001 Sniper Rifle. Your Ghost is also with you. He can answer any questions you may have.")
 
-    elif inputTwo in ["ready weapon", "weapon", "equip weapon"]:
+            ##Ready Weapon tree
+    elif inputTwo in ["ready weapon","ready", "weapon", "equip weapon"]:
         inputWeapon=input("Which weapon do you equip? >>> ").lower()
         if inputWeapon in ["mida", "multi-tool", "mida multi-tool", "scout rifle"]:
             print("MIDA Multi-Tool equipped.")
             inventory["MIDA"] = True
             inventory["LDR"] = False
-
-
         elif inputWeapon in ["ldr", "ldr-5001", "sniper rifle"]:
             print("LDR-5001 equipped.")
             inventory["MIDA"] = False
             inventory["LDR"] = True
-
         else:
             print("No weapon equipped.")
 
+            ##Ghost Rune
     elif inputTwo in ["ghost rune", "check rune", "rune", "inspect rune", "look rune"]:
         partTwo=False
 
@@ -89,48 +90,49 @@ while partThree:
         print("-Use a Void ability-")
         print("Thrall left: " , Thrall)
 
-            ##Equip Weapon tree
+
         inputThree=input("What will you do? >>> ").lower()
-        if inputThree in ["ready weapon", "equip weapon"]:
+                ##Ready Weapon tree
+        if inputThree in ["ready weapon", "equip weapon", "ready"]:
             inputWeapon=input("Which weapon do you equip? >>> ").lower()
             if inputWeapon in ["mida", "multi-tool", "mida multi-tool", "scout rifle"]:
                 print("MIDA Multi-Tool equipped.")
                 inventory["MIDA"] = True
                 inventory["LDR"] = False
                 battleOne -= 1
-
             elif inputWeapon in ["ldr", "ldr-5001", "sniper rifle"]:
                 print("LDR-5001 equipped.")
                 inventory["MIDA"] = False
                 inventory["LDR"] = True
                 battleOne -= 1
-
             else:
                 print("No weapon equipped.")
+
                 ##Void Ability tree
         elif inputThree in ["use void", "void", "use void ability", "use ability"]:
             inputVoid=input("Which ability do you use? >>> ").lower()
             if inputVoid in ["axion bolt", "bolt", "grenade"]:
-                if Thrall != 1:
-                    print("Your Axion Bolt is flung in front of you, roughly in the direction of the incoming Thrall. 2 bolts spawn in the radius and seek out two of the Thrall. They erupt in a void explosion.")
-                    Thrall -= 2
-                    print(Thrall)
-                    battleOne -= 2
-                    abilities["Axion Bolt"] -= 1
-                    print(abilities)
-                    continue
-
-                elif Thrall == 1:
-                    print ("Your Axion Bolt is flung in front of you, in the direction of the last Thrall. It stumbles at the sight of your grenade, aware of its power. The bolt promptly consumes the creature, killing it.")
-                    Thrall -= 1
-                    abilities["Axion Bolt"] -= 1
-                    print(abilities)
-                    continue
-
+                if abilities["Axion Bolt"]!=0:
+                    if Thrall != 1:
+                        print("Your Axion Bolt is flung in front of you, roughly in the direction of the incoming Thrall. 2 bolts spawn in the radius and seek out two of the Thrall. They erupt in a void explosion.")
+                        Thrall -= 2
+                        print(Thrall)
+                        battleOne -= 2
+                        abilities["Axion Bolt"] -= 1
+                        print(abilities)
+                        continue
+                    elif Thrall == 1:
+                        print ("Your Axion Bolt is flung in front of you, in the direction of the last Thrall. It stumbles at the sight of your grenade, aware of its power. The bolt promptly consumes the creature, killing it.")
+                        Thrall -= 1
+                        abilities["Axion Bolt"] -= 1
+                        print(abilities)
+                        continue
+                elif abilities["Axion Bolt"]==0:
+                    print("Your Axion Bolt charges are depleted")
             elif inputVoid in ["nova bomb", "nova", "bomb", "void bomb", "one round purple boi"]:
                 print("You jump up and summon an intense orb of Void energy between your palms, proppelling it towards the incoming Thrall. They don't stand a chance. The bomb decimates them, leaving no trace.")
                 Thrall -= 5
-
+                abilities["Nova Bomb"]-=1
             else:
                 print("-Invalid action, please try again-")
 
@@ -161,10 +163,11 @@ while partThree:
                     continue
                 elif Thrall == 1:
                     print("The last Thrall charges towards you, and without scoping in, you take a point blank shot at its head. Your bullet rings loudly through the valley. You reload your clip.")
+                    Thrall -= 1
                     continue
+
             else:
                 print("-You have no weapon equipped-")
-
 
         else:
             print("-Invalid action, please try again-")
@@ -181,7 +184,7 @@ while partThree:
 print("You move slowly through the tomb, past the chamber. All of a sudden you see two blaster shots from opposite directions coming towards you. You dodge out of the way, and get a glimpse at your foes. Two Hive Acolytes and a Knight.")
 ##Part Four (Acolyte battle)
 while partFour:
-    if Acolytes <= 0 and Knight <= 0:
+    if battleTwoEnemies["Knight"]<=0 and battleTwoEnemies["AcolyteOne"]<=0 and battleTwoEnemies["AcolyteTwo"]<=0:
             print("You've cleared the wave of enemies. You make your way deeper into the tomb.")
             partFour=False
 
@@ -192,3 +195,114 @@ while partFour:
         print("-Talk to your Ghost-")
         print("-Take Cover-")
         inputFour = input("What will you do? >>> ").lower()
+
+            ##Ready Weapon tree
+        if inputFour in ["ready weapon", "equip weapon", "ready"]:
+            inputWeapon=input("Which weapon do you equip? >>> ").lower()
+            if inputWeapon in ["mida", "multi-tool", "mida multi-tool", "scout rifle"]:
+                print("MIDA Multi-Tool equipped.")
+                inventory["MIDA"] = True
+                inventory["LDR"] = False
+                battleTwo -= 1
+            elif inputWeapon in ["ldr", "ldr-5001", "sniper rifle"]:
+                print("LDR-5001 equipped.")
+                inventory["MIDA"] = False
+                inventory["LDR"] = True
+                battleTwo -= 1
+            else:
+                print("No weapon equipped.")
+
+                ##Cover tree
+        elif inputFour in ["cover", "take cover", "get into cover", "go to cover"]:
+            if cover==False:
+                print("You hover towards a pillar to your right. It looks thick enough to absorb a lot of damage from enemy fire.")
+                battleTwo += 3
+                cover=True
+            else:
+                print("You're already in cover.")
+
+                ##Void Ability tree
+        elif inputFour in ["use void", "void", "use void ability", "use ability"]:
+            inputVoid=input("Which ability do you use? >>> ").lower()
+            if inputVoid in ["axion bolt", "bolt", "grenade"]:
+                if abilities["Axion Bolt"]!=0:
+                    inputTarget=input("Do you target the Acolytes or the Knight? >>> ").lower()
+                    if inputTarget in ["acolytes"]:
+                        if battleTwoEnemies["AcolyteOne"]<=0 and battleTwoEnemies["AcolyteTwo"]<=2:
+                            print("Your Axion Bolt hits the remaining Acolyte. They explode with Void energy.")
+                            battleTwoEnemies["AcolyteTwo"]-=2
+                            abilities["Axion Bolt"]-=1
+                            continue
+                        elif battleTwoEnemies["AcolyteOne"]<=2 and battleTwoEnemies["AcolyteTwo"]<=0:
+                            print("Your Axion Bolt hits the remaining Acolyte. They explode with Void energy.")
+                            battleTwoEnemies["AcolyteOne"]-=2
+                            abilities["Axion Bolt"]-=1
+                            continue
+                        elif battleTwoEnemies["AcolyteOne"]<=2 and battleTwoEnemies["AcolyteTwo"]<=2:
+                            if battleTwoEnemies["Knight"]>=4:
+                                print ("Your Axion Bolt hits both Acolytes. It's just enough to kill them. The void explosion shrapnels towards the Knight, scraping its armour")
+                                battleTwoEnemies["AcolyteOne"]-=2
+                                battleTwoEnemies["AcolyteTwo"]-=2
+                                battleTwoEnemies["Knight"]-=1
+                                abilities["Axion Bolt"]-=1
+                                continue
+                            elif battleTwoEnemies["Knight"]<=3:
+                                print ("Your Axion Bolt hits both Acolytes. It's just enough to kill them. They explode with void energy.")
+                                battleTwoEnemies["AcolyteOne"]-=2
+                                battleTwoEnemies["AcolyteTwo"]-=2
+                                abilities["Axion Bolt"]-=1
+                                continue
+                        elif battleTwoEnemies["AcolyteOne"]<=0 and battleTwoEnemies["AcolyteTwo"]<=0:
+                            print("There are no Acolytes left.")
+                            continue
+                    elif inputTarget in ["knight"]:
+                        if battleTwoEnemies["Knight"]>=3:
+                            print("The Axion Bolt lets out one charge, targeted at the Knight. It makes contact, but only staggers the Knight. It boasts by swinging its sword.")
+                            battleTwoEnemies["Knight"]-=2
+                            abilities["Axion Bolt"]-=1
+                            continue
+                        if battleTwoEnemies["Knight"]<=2:
+                            print("The Axion Bolt lets out on charge, targeted at the Knight. The creature explodes with Void energy and no trace is left of it")
+                            battleTwoEnemies["Knight"]-=2
+                            abilities["Axion Bolt"]-=1
+                elif abilities["Axion Bolt"]==0:
+                    print("Your Axion Bolt charges are depleted.")
+            elif inputVoid in ["nova bomb", "nova", "bomb", "void bomb", "one round purple boi"]:
+                if abilities["Nova Bomb"]!=0:
+                    if battleTwoEnemies["Knight"]>=4 and battleTwoEnemies["AcolyteOne"]>=1 and battleTwoEnemies["AcolyteTwo"]>=1:
+                        print("You summon a sphere of pure void energy, and launch it towards your foes. The Acolytes are evicerated, and the Knight is also badly wounded, but is still able to fight.")
+                        battleTwoEnemies["Knight"]-=3
+                        battleTwoEnemies["AcolyteOne"]-=2
+                        battleTwoEnemies["AcolyteTwo"]-=2
+                        abilities["Nova Bomb"]-=1
+                        continue
+                    if battleTwoEnemies["Knight"]>=4 and battleTwoEnemies["AcolyteOne"]<=0 and battleTwoEnemies["AcolyteTwo"]<=0:
+                        print("You summon a sphere of pure void energy, and launch it towards your foe. The Knight takes the full force of your Nova Bomb, but is still just strong enough to keep fighting.")
+                        battleTwoEnemies["Knight"]-=3
+                        continue
+                    if battleTwoEnemies["Knight"]<=3 and battleTwoEnemies["AcolyteOne"]>=1 and battleTwoEnemies["AcolyteTwo"]>=1:
+                        print("You summon a sphere of pure void energy, and launch it towards your foes. It obliterates them completely, consuming them within a purple dome of light.")
+                        battleTwoEnemies["Knight"]-=3
+                        battleTwoEnemies["AcolyteOne"]-=2
+                        battleTwoEnemies["AcolyteTwo"]-=2
+                        abilities["Nova Bomb"]-=1
+                        continue
+                    if battleTwoEnemies["Knight"]<=3 and battleTwoEnemies["AcolyteOne"]<=0 and battleTwoEnemies["AcolyteTwo"]<=0:
+                        print("You summon a sphere of pure void energy, and launch it towards your foe. The Knight is consumed by your Nova Bomb completely, and explodes into nothing.")
+                        battleTwoEnemies["Knight"]-=3
+                        abilities["Nova Bomb"]-=1
+                        continue
+                    if battleTwoEnemies["Knight"]<=0 and battleTwoEnemies["AcolyteOne"]>=1 and battleTwoEnemies["AcolyteTwo"]>=1:
+                        print("You summon a sphere of pure void energy, and launch it towards your foes. The remaining Acolytes are evicerated with no trace left.")
+                        battleTwoEnemies["AcolyteOne"]-=2
+                        battleTwoEnemies["AcolyteTwo"]-=2
+                        abilities["Nova Bomb"]-=1
+                        continue
+            else:
+                print("-Invalid action, please try again-")
+
+print("You arrive inside the tomb's Throne Room. There you see a locked chest, with four shapes portruding from it. It looks like they function as the keys to this chest, but you aren't certain.")
+print("-All abilites have been regenerated-")
+abilities["Axion Bolt"]=2
+abilities["Nova Bomb"]=1
+print(abilities)
